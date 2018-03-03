026a861200e51e277e89dc6af11f196a48d3ffa1
Morten Wehlast Jørgensen <Morten@Mortens-MacBook-Pro-7.local>
2018-03-03 13:46:05 +0100

    Updates

--- tomography_project.py
+++ tomography_project.py
@@ -6,9 +6,9 @@ Created on Tue Feb 20 09:41:52 2018
 @author: Morten
 """
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from paralleltomo import paralleltomo
-
+import phantom
 
 
 ### Matrix A
@@ -71,3 +71,93 @@ rank_A_1a = np.linalg.matrix_rank(A_1a)
 rank_A_1b = np.linalg.matrix_rank(A_1b)
 rank_A_2a = np.linalg.matrix_rank(A_2a)
 rank_A_2b = np.linalg.matrix_rank(A_2b)
+
+
+
+### Verification
+
+# Create shepp logan phantom
+X = phantom.shepp2d(N)
+x = np.reshape(X, (N*N, 1))
+b = np.dot(A,x)
+
+# Numerical experiments
+# Create matrix with matrices with full rank + condition numbers
+matrices = [A_1a, A_1b, A_2b]
+cond_num = [cond_A_1a, cond_A_1b, cond_A_2b]
+
+# Pre-calculate inverse of matrices
+matrices_inv = np.linalg.inv(matrices)
+
+
+# Allocate memory for relative error measurements and noise
+# 10 tests, 6 noise levels (magnitude of 10 between steps)
+n_matrix = len(matrices)
+n_levels = 6
+n_tests = 10
+rel_error_mat = np.zeros([n_matrix, n_levels])
+max_error_mat = np.zeros([n_matrix, n_levels])
+
+
+# Create noise for matrices
+# Set seed
+np.random.seed(142)
+noise_mat = np.zeros([4096,n_tests,n_levels])
+
+for i in range(0, n_levels):
+    noiselevel = 10**(-(i+1))
+    
+    for j in range(0, n_tests):
+        noise_mat[:,j,i] = (noiselevel*np.random.normal(0,1,b.shape)).squeeze()
+      
+
+
+# Calculate errors for matrices. Error calcalutated as mean over all 10 tests
+
+for i in range(0, n_levels):
+    
+    #Loop over matrices
+    for j in range(0, n_matrix):
+        A           = matrices[j]
+        A_inv       = matrices_inv[j]
+        b           = np.dot(A,x)
+        kappa       = cond_num[j]
+        errors      = np.zeros([n_tests, 1])
+        upper_bound = np.zeros([n_tests, 1])
+        
+        # Calc errors for each noise vector at level i
+        for k in range(0, n_tests):
+            btilde         = b + noise_mat[:,k,i]
+            xtilde         = np.dot(A_inv, btilde)
+            errors[k]      = np.linalg.norm(x-xtilde)/np.linalg.norm(x)
+            #upper_bound[k] = kappa*np.linalg.norm(b-btilde)/np.linalg.norm(b)
+            
+        #max_error_mat[j,i] = np.mean(upper_bound)
+        rel_error_mat[j,i] = np.mean(errors)
+
+        
+
+#for i in range(0, n_matrix):
+#    # Assign matrix to be tested
+#    A = matrices[i]
+#    b = np.dot(A,x)
+#    kappa = np.linalg.cond(A)
+#    
+#    for j in range(0, n_levels):
+#        noiselevel = 10**(-(j+1))
+#        print(noiselevel)
+#        btilde = b + noiselevel*np.random.normal(0,1,b.shape)
+#        xtilde = np.linalg.solve(A, btilde)
+#        
+#        # add relative error and max possible error to matrices
+#        max_error_mat[i,j] = kappa*np.linalg.norm(b-btilde)/np.linalg.norm(b)
+#        rel_error_mat[i,j] = np.linalg.norm(x-xtilde)/np.linalg.norm(x)
+
+
+
+
+# Plot the reconstruction
+plt.imshow(np.reshape(xtilde, (N, N)))
+
+
+### Numerical experiments
